b4c9b00b94dab0fc76037d312066db148f97134d
jiangshiyu <3434481891@qq.com>
2022-06-16 17:04:42 +0800

    initial project

---
+++ app/src/main/java/com/base/mvi_arch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.base.mvi_arch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
